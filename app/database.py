d8ce551f00ad207df6ed5eb452994a5dd1fd435b
VF <77busina@gmail.com>
2024-09-30 15:46:04 +0400

    init: docker

--- app/database.py
+++ app/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:3904@localhost/e-store"
+DATABASE_URL = "postgresql://postgres:3904@db/e-store"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -2,20 +2,22 @@ import uvicorn
 from typing import List
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
-
 from app.crud import update_order_status
-from app.schemas import OrderStatusUpdate
-
-'''from . import crud, models, schemas'''
 from app import crud, models, schemas
 from app.database import SessionLocal, engine
 
+# Initialize database and create tables
 models.Base.metadata.create_all(bind=engine)
 
+# Initialize FastAPI app
 app = FastAPI(debug=True)
 
 
 def get_db():
+    """
+    Dependency to provide a database session.
+    Ensures that the session is properly closed after the request.
+    """
     db = SessionLocal()
     try:
         yield db
@@ -23,61 +25,78 @@ def get_db():
         db.close()
 
 
-# Эндпоинт для создания товара
 @app.post("/products", response_model=schemas.Product)
 def create_product(product: schemas.ProductCreate, db: Session = Depends(get_db)):
+    """
+    Create a new product.
+    """
     return crud.create_product(db=db, product=product)
 
 
-# Эндпоинт для получения списка товаров
 @app.get("/products", response_model=List[schemas.Product])
-def get_product_list(db: Session = Depends(get_db)):
+def list_products(db: Session = Depends(get_db)):
+    """
+    Get a list of all products.
+    """
     return crud.get_products(db)
 
 
-# Эндпоинт для получения информации о товаре по id
 @app.get("/products/{id}", response_model=schemas.Product)
-def find_product(id: int, db: Session = Depends(get_db)):
+def get_product(id: int, db: Session = Depends(get_db)):
+    """
+    Get details of a product by its ID.
+    """
     return crud.get_product_by_id(db=db, product_id=id)
 
 
-# Эндпоинт для обновления информации о товаре (PUT /products/{id})
 @app.put("/products/{id}", response_model=schemas.Product)
 def update_product(id: int, product: schemas.ProductUpdate, db: Session = Depends(get_db)):
+    """
+    Update product details by its ID.
+    """
     return crud.update_product(db=db, product=product, product_id=id)
 
 
-# Эндпоинт для удаления товара (DELETE /products/{id})
 @app.delete("/products/{id}", response_model=schemas.Product)
 def delete_product(id: int, db: Session = Depends(get_db)):
+    """
+    Delete a product by its ID.
+    """
     return crud.delete_product(db=db, product_id=id)
 
 
-# Эндпоинт для создания заказа
 @app.post("/orders", response_model=schemas.Order)
 def create_order(order: schemas.OrderCreate, db: Session = Depends(get_db)):
+    """
+    Create a new order.
+    """
     return crud.create_order(db=db, order=order)
 
 
-
-# Эндпоинт для получения списка заказов (GET /orders).
 @app.get("/orders", response_model=List[schemas.Order])
-def get_order_list(db: Session = Depends(get_db)):
+def list_orders(db: Session = Depends(get_db)):
+    """
+    Get a list of all orders.
+    """
     return crud.get_orders(db)
 
 
-# Эндпоинт для получения информации о заказе по id (GET /orders/{id}).
 @app.get("/orders/{id}", response_model=schemas.Order)
-def read_order(id: int, db: Session = Depends(get_db)):
+def get_order(id: int, db: Session = Depends(get_db)):
+    """
+    Get details of an order by its ID.
+    """
     return crud.get_order_by_id(db=db, order_id=id)
 
 
-# Эндпоинт для обновления статуса заказа (PATCH /orders/{id}/status).
 @app.patch("/orders/{order_id}/status")
-def change_order_status(order_id: int, status_update: OrderStatusUpdate, db: Session = Depends(get_db)):
+def update_order_status_endpoint(order_id: int, status_update: schemas.OrderStatusUpdate, db: Session = Depends(get_db)):
+    """
+    Update the status of an order by its ID.
+    """
     return update_order_status(db, order_id, status_update.new_status)
 
 
 if __name__ == "__main__":
-    # Запуск приложения через Uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8001, reload=False)
+    # Run the FastAPI app with Uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=False)
